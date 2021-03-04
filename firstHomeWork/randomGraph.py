3115c49232b4287f970d5c1853d703d9fecbf15e
Treciokas <to.treciokas@gmail.com>
2021-03-04 09:36:31 +0200

    Code improvements

--- firstHomeWork/randomGraph.py
+++ firstHomeWork/randomGraph.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-N = 1000
-index = pd.date_range('2020-3-1', periods=N, freq='B')
-data = (np.random.random((N, 1))-0.5).cumsum(axis=0)
+index = pd.date_range('2020-3-1', periods=1000, freq='B')
+data = (np.random.random((1000, 1))-0.5).cumsum(axis=0)
 df = pd.DataFrame(data, index=index, columns=['kaina'])
 print(df)
 df.plot()
+plt.grid()
 plt.show()
 # add grid
