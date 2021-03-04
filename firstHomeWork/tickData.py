b29c949c5490824c2128c7577b6f68bc0e6696b6
Treciokas <to.treciokas@gmail.com>
2021-03-04 09:24:55 +0200

    Code improvements

--- firstHomeWork/tickData.py
+++ firstHomeWork/tickData.py
@@ -6,14 +6,14 @@ csv_data = pd.read_csv('../resource/EFA.USUSD_Ticks_17.02.2021-17.02.2021.csv')
 csv_data['Local time'] = pd.to_datetime(csv_data['Local time'], format="%d.%m.%Y %H:%M:%S.%f GMT+0200")
 csv_data = csv_data.set_index('Local time')
 
-rowCount = csv_data.shape[0]
+rowCount = csv_data.shape[0] + 1
 x = np.array(range(1, rowCount))
 csv_data['Indeksas'] = x
 csv_data = csv_data.set_index('Indeksas')
 
 plt.plot(csv_data.head(100))
 plt.legend(['Ask', 'Bid'])
-plt.xlabel('Laikas')
+plt.xlabel('Kiekis')
 plt.ylabel('Kaina')
 plt.grid()
 plt.show()
